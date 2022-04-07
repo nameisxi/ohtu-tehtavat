43d7d78c9b853b22fa3a2a70e7ba7a05ccfbfabc
Tuomas Peltonen <tuomas.j.j.peltonen@gmail.com>
2022-04-07 18:52:57 +0900

    Testi tuotteen lisayksesta ja poistosta lisatty

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -102,4 +102,12 @@ class TestOstoskori(unittest.TestCase):
 
         ostos = self.kori.ostokset()[0]
         self.assertEqual(ostos.lukumaara(), 1)
+
+    def test_koriin_lisatty_tuote_poistetaan_ja_kori_tyhja(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.poista_tuote(maito)
+
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 0)
         
