5bb80bc59e4e828cdb1b75a6acf3be8c4ce802ec
Tuomas Peltonen <tuomas.j.j.peltonen@gmail.com>
2022-04-07 18:39:22 +0900

    Testi kahden ostoksen lisayksen tarkastamiselle lisatty

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -63,3 +63,12 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(ostos.tuotteen_nimi(), "Maito")
         self.assertEqual(ostos.lukumaara(), 1)
+
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        hiiva = Tuote("Hiiva", 7)
+        self.kori.lisaa_tuote(hiiva)
+
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 2)
