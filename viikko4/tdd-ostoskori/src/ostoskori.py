2ada682a32b4bd2e31df457ed3408b29a1842293
Tuomas Peltonen <tuomas.j.j.peltonen@gmail.com>
2022-04-07 18:17:04 +0900

    lisatty ostoskori testi joka kertoo ostoskorin alkuperaisen arvon rahassa ja lukumaarassa

---
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -0,0 +1,31 @@
+from tuote import Tuote
+from ostos import Ostos
+
+class Ostoskori:
+    def __init__(self):
+        # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
+        self._ostokset = []
+
+    def tavaroita_korissa(self):
+        pass
+        # kertoo korissa olevien tavaroiden lukumäärän
+        # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
+        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
+
+    def hinta(self):
+        return 0
+
+    def lisaa_tuote(self, lisattava: Tuote):
+        # lisää tuotteen
+        pass
+
+    def poista_tuote(self, poistettava: Tuote):
+        # poistaa tuotteen
+        pass
+
+    def tyhjenna(self):
+        pass
+        # tyhjentää ostoskorin
+
+    def ostokset(self):
+        return self._ostokset
