1ffdd2071e39d92db6e479544fa6e65e6fcd34c6
Tuomas Peltonen <tuomas.j.j.peltonen@gmail.com>
2022-04-11 16:20:23 +0900

    viikko 5 tehtava 3

---
+++ viikko5/laskin/src/kayttoliittyma.py
@@ -0,0 +1,97 @@
+from enum import Enum
+from tkinter import ttk, constants, StringVar
+
+
+class Komento(Enum):
+    SUMMA = 1
+    EROTUS = 2
+    NOLLAUS = 3
+    KUMOA = 4
+
+
+class Kayttoliittyma:
+    def __init__(self, sovellus, root):
+        self._sovellus = sovellus
+        self._root = root
+
+    def kaynnista(self):
+        self._tulos_var = StringVar()
+        self._tulos_var.set(self._sovellus.tulos)
+        self._syote_kentta = ttk.Entry(master=self._root)
+
+        tulos_teksti = ttk.Label(textvariable=self._tulos_var)
+
+        summa_painike = ttk.Button(
+            master=self._root,
+            text="Summa",
+            command=lambda: self._summa()
+        )
+
+        erotus_painike = ttk.Button(
+            master=self._root,
+            text="Erotus",
+            command=lambda: self._erotus()
+        )
+
+        self._nollaus_painike = ttk.Button(
+            master=self._root,
+            text="Nollaus",
+            state=constants.DISABLED,
+            command=lambda: self._nollaus()
+        )
+
+        self._kumoa_painike = ttk.Button(
+            master=self._root,
+            text="Kumoa",
+            state=constants.DISABLED,
+            command=lambda: self._kumoa()
+        )
+
+        tulos_teksti.grid(columnspan=4)
+        self._syote_kentta.grid(columnspan=4, sticky=(constants.E, constants.W))
+        summa_painike.grid(row=2, column=0)
+        erotus_painike.grid(row=2, column=1)
+        self._nollaus_painike.grid(row=2, column=2)
+        self._kumoa_painike.grid(row=2, column=3)
+
+    def _syote(self):
+        arvo = 0
+
+        try:
+            arvo = int(self._syote_kentta.get())
+        except Exception as e:
+            print(e)
+        
+        return arvo 
+
+    def _paivita_tila(self):
+        self._kumoa_painike["state"] = constants.NORMAL
+
+        if self._sovellus.tulos == 0:
+            self._nollaus_painike["state"] = constants.DISABLED
+        else:
+            self._nollaus_painike["state"] = constants.NORMAL
+
+        self._syote_kentta.delete(0, constants.END)
+        self._tulos_var.set(self._sovellus.tulos)
+
+
+    def _summa(self):
+        arvo = _syote()
+        self._sovellus.plus(arvo)
+        self._paivita_tila()
+
+    def _erotus(self):
+        arvo = _syote()
+        self._sovellus.miinus(arvo)
+        self._paivita_tila()
+
+
+    def _nollaus(self):
+        arvo = _syote()
+        self._sovellus.nollaa(arvo)
+        self._paivita_tila()
+
+    def _kumous():
+        pass
+
